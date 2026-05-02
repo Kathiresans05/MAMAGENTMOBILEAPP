d97729b179ec10dcb593099b5df005799433026a
Kathiresan05 <kathiresans0502@gmail.com>
2026-05-02 12:46:07 +0530

    chore: update API URL to production Render endpoint

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://10.0.2.2:5000/api', // Local Backend
+    baseURL: 'https://agentmobileapp.onrender.com/api', // Render Production Backend
     headers: {
         'Content-Type': 'application/json',
     },
